900d68eeae346b27314e22da5aae7697018d738f
liben <714376485@qq.com>
2021-12-28 14:22:10 +0800

    1

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ const http = require('http');
 
 const hostname = '127.0.0.1';
 const port = 3000;
-const {writeFile} = require('fs/promises')
+const {writeFile} = require('fs')
 
 const server = http.createServer((req, res) => {
-
-    writeFile("temp.txt","temp")
 
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html; charset=utf-8');
